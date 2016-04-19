cfa7b09f8cfb34e41e648016ac50572f8bb0df36
Goksel Koksal <gokselkoksal@gmail.com>
2016-04-19 21:56:50 +0300

    Run `pod install`

---
+++ Example/Pods/Target Support Files/OpKit/OpKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_OpKit : NSObject
+@end
+@implementation PodsDummy_OpKit
+@end
---
+++ Example/Pods/Target Support Files/OpKit/OpKit-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double OpKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char OpKitVersionString[];
+
